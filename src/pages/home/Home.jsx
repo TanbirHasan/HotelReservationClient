6e52c611b2af386b0c48bcee4d9c8843e5eaaf15
unknown <mdtanbir576@gmail.com>
2022-06-08 11:32:43 +0600

    property and property list

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,11 +2,27 @@ import React from 'react';
 
 import List from '../list/List';
 import "./Home.css"
+import Navbar from "../../components/navbar/Navbar"
+import Header from '../../components/header/Header';
+import Featured from '../../components/Feature/Featured';
+import Footer from '../../components/footer/Footer';
+import PropertyList from '../../components/PropertyList/PropertyList';
 
 const Home = () => {
     return (
         <div>
-         <List/>
+            <Navbar />
+            <Header />
+            <div className="homeContainer">
+                <Featured/>
+                <h1 className='homeTitle'>Browse by property type</h1>
+                <PropertyList/>
+            </div>
+
+            <Footer/>
+
+            
+        
             
         </div>
     );
--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Header from '../../components/header/Header';
-import Navbar from '../../components/navbar/Navbar';
+
 
 const List = () => {
     return (
         <div>
-            <Navbar/>
-            <Header type="list"/>
+           
         </div>
     );
 };
